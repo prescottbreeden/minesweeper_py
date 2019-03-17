1d81eeb81cd7258f8ade0ac866d37d3a5f1f1e82
prescottbreeden <prescott.breeden@gmail.com>
2019-03-17 14:46:27 -0700

    game.py bug fixes
    added board size to make board creation more dynamic, fixed control flow
    logic bugs that were causing out of bounds error, fixed missing return
    in recurssive add bomb function

--- Models/game.py
+++ Models/game.py
@@ -4,17 +4,18 @@ from Models.position import Position
 from random import randint
 
 class Game:
-    def __init__(self, size):
-        self.total_bombs = size
+    def __init__(self, bombs):
+        self.total_bombs = bombs
+        self.size = bombs-2
         self.board = [] 
         self.create_board()
         self.generate_bombs()
 
 
     def create_board(self):
-        for x in range(self.total_bombs):
+        for y in range(self.size):
             row = []
-            for y in range(self.total_bombs):
+            for x in range(self.size):
                 row.append(Square(x,y))
             self.board.append(row)
 
@@ -22,32 +23,35 @@ class Game:
     def generate_bombs(self):
         bombs = []
         while len(bombs) < self.total_bombs:
-            loc = self.add_bomb()
-            bombs.append(loc)
+            sq = self.add_bomb()
+            bombs.append(sq)
         print(bombs)
 
 
     def add_warnings(self, pos):
         # increment sides
         if pos.y > 0: self.inc_warn_top(pos)
-        if pos.y < 9: self.inc_warn_bottom(pos)
-        if pos.x < 9: self.inc_warn_right(pos)
         if pos.x > 0: self.inc_warn_left(pos)
+        if pos.y < self.size-1: self.inc_warn_bottom(pos)
+        if pos.x < self.size-1: self.inc_warn_right(pos)
 
         # increment diagonals
         if pos.x > 0 and pos.y > 0: 
             self.inc_warn_top_left(pos)
-        if pos.x < 9 and pos.y < 0: 
+            
+        if pos.x < self.size-1 and pos.y > 0: 
             self.inc_warn_top_right(pos)
-        if pos.x < 9 and pos.y < 9: 
+            
+        if pos.x < self.size-1 and pos.y < self.size-1: 
             self.inc_warn_bottom_right(pos)
-        if pos.x > 0 and pos.y < 9: 
+
+        if pos.x > 0 and pos.y < self.size-1: 
             self.inc_warn_bottom_left(pos)
 
 
     def add_bomb(self):
-        x = randint(0,9)
-        y = randint(0,9)
+        x = randint(0,self.size-1)
+        y = randint(0,self.size-1)
         pos = Position(x,y)
         sq = self.find_square(pos)
         if not sq.bomb:
@@ -55,7 +59,7 @@ class Game:
             self.add_warnings(pos)
             return sq
         else:
-            self.add_bomb() 
+            return self.add_bomb() 
 
 
     # relational coordinate map:
@@ -64,7 +68,7 @@ class Game:
     # right:        y   x+1
     # bottom right: y+1 x+1
     # bottom:       y+1 x
-    # bottom left:  y-1 x-1
+    # bottom left:  y+1 x-1
     # left:         y   x-1
     # top left:     y-1 x-1
 
@@ -76,8 +80,8 @@ class Game:
 
 
     def inc_warn_bottom_left(self, pos):
-        pos.y -= 1
-        pos.x += 1
+        pos.y += 1
+        pos.x -= 1
         sq = self.find_square(pos)
         sq.warning += 1
 
--- Models/position.py
+++ Models/position.py
@@ -2,3 +2,7 @@ class Position:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    
+    def __repr__(self):
+        return "x:%i, y:%i" % (self.x, self.y)
